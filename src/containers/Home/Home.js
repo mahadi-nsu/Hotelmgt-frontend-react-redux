af0f9da6278e707073aa00ac8ef45cfb2ba33864
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-24 12:22:11 +0600

    boilerplate reducers deleted

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import withErrorModal from "../../components/HOC/withErrorModal/withErrorModal";
 import PersonCard from "../../components/PersonCard/PersonCard";
 import EditModal from "../../components/UI/EditModal/EditModal";
 import { useDispatch, useSelector } from "react-redux";
@@ -18,28 +17,28 @@ function Home() {
     dispatch(allUsersData());
   }, [dispatch]);
 
-
   return (
     <div className="py-20">
       <div className="grid grid-cols-12 place-items-center px-5 2xl:px-10 gap-4 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg 2xl:max-w-screen-2xl mx-auto my-0">
-        {users && users.length > 0 ? users.map((person) => (
-          <PersonCard
-            person={person}
-            setShowEditModal={setShowEditModal}
-            showEditModal={showEditModal}
-            setselectedPerson={setselectedPerson}
-            key={person._id}
-          />
-        )) : isUsersLoading ?
-
+        {users && users.length > 0 ? (
+          users.map((person) => (
+            <PersonCard
+              person={person}
+              setShowEditModal={setShowEditModal}
+              showEditModal={showEditModal}
+              setselectedPerson={setselectedPerson}
+              key={person._id}
+            />
+          ))
+        ) : isUsersLoading ? (
           <div className="text-center text-2xl text-gray-600 col-span-12 h-40  flex justify-center items-center">
             <p>Loading....</p>
           </div>
-          :
+        ) : (
           <div className="text-center text-2xl text-gray-600 col-span-12 h-40  flex justify-center items-center">
             <p>List Empty, Add New Person</p>
           </div>
-        }
+        )}
       </div>
       {showEditModal && (
         <EditModal
@@ -53,4 +52,4 @@ function Home() {
   );
 }
 
-export default withErrorModal(Home);
+export default Home;
--- src/store/actions/index.js
+++
@@ -1,2 +0,0 @@
-export { login } from './auth/login';
-export { openErrorModal, closeErrorModal } from './errorModal/errorModal';
