22f05c92facd5e0d80ac0c29f0f6ea1c7255c028
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-02-24 13:06:33 +0600

    more clean up and header added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HeaderMerchants from "./components/Headers/Header";
-import Login from './containers/Login/Login';
-import Footer from './components/Footer/Footer';
-import Home from './containers/Home/Home';
+import Login from "./containers/Login/Login";
+import Home from "./containers/Home/Home";
 
 function App() {
-
   return (
     <Router>
       <HeaderMerchants />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
-      {/* <Footer /> */}
     </Router>
   );
 }
